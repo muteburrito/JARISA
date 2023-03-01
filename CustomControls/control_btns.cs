532e7ea517db1dccc0309d0bdb989f0e8810e250
ckulkarni <kulkarnichinmay65@gmail.com>
2023-03-01 23:38:13 +0530

    Adding a basic window layout

---
+++ CustomControls/control_btns.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+public class control_btns : Button
+{
+    private Color buttonColor;
+
+    public control_btns(Color color)
+    {
+        buttonColor = color;
+        FlatStyle = FlatStyle.Flat;
+        BackColor = buttonColor;
+        ForeColor = Color.White;
+        Font = new Font("Arial", 12, FontStyle.Bold);
+        Size = new Size(50, 50);
+        Text = "";
+        Padding = new Padding(0);
+        FlatAppearance.BorderSize = 0;
+        Region = new Region(new Rectangle(0, 0, Width, Height));
+    }
+
+    protected override void OnPaint(PaintEventArgs e)
+    {
+        base.OnPaint(e);
+        e.Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+        using (var brush = new SolidBrush(buttonColor))
+        {
+            e.Graphics.FillEllipse(brush, new RectangleF(0, 0, Width, Height));
+        }
+    }
+
+    protected override void OnResize(EventArgs e)
+    {
+        base.OnResize(e);
+        Region = new Region(new Rectangle(0, 0, Width, Height));
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JARISA
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            GraphicsPath path = new GraphicsPath();
+            using (Graphics g = CreateGraphics())
+            {
+                g.SmoothingMode = SmoothingMode.AntiAlias;
+            }
+        }
+
+        private bool isDragging = false;
+        private Point lastLocation;
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            isDragging = true;
+            lastLocation = e.Location;
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDragging)
+            {
+                this.Location = new Point(
+                    (this.Location.X - lastLocation.X) + e.X,
+                    (this.Location.Y - lastLocation.Y) + e.Y);
+                this.Update();
+            }
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            isDragging = false;
+        }
+    }
+}
